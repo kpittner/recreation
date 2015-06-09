2a210056ebfb01d9d2e3d5d21fa4d725ca817362
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-06-09 17:15:12 -0400

    Work so far

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  var demoNameAndDate = _.map(myDemos, function(el) {
+    return {
+       name: el.name,
+       date: el.updated_at
+     }
+  });
+
+  var demos = _.template($('#myDemos').html());
+
+  _.each(demoNameAndDate, function(el){
+    $('.repos').append(demos(el));
+  });
+
+});
