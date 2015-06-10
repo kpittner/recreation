7ffa6834c475a3c36ad840b54668f09e44fc4bbf
Kelsey Pittner <KelseyJade@Kelseys-MacBook-Air.local>
2015-06-10 00:24:28 -0400

    Progress

--- data.js
+++ data.js
@@ -817,7 +817,7 @@ var myDemos = [
     "watchers": 0,
     "default_branch": "master"
   }
-]
+].reverse("");
 
 
 var myEvents = [
@@ -1586,5 +1586,15 @@ var myEvents = [
 ]
 
 var myOrgs = [
-
+  {
+  "login": "TIY-Charleston-Front-End-May2015",
+  "id": 12236563,
+  "url": "https://api.github.com/orgs/TIY-Charleston-Front-End-May2015",
+  "repos_url": "https://api.github.com/orgs/TIY-Charleston-Front-End-May2015/repos",
+  "events_url": "https://api.github.com/orgs/TIY-Charleston-Front-End-May2015/events",
+  "members_url": "https://api.github.com/orgs/TIY-Charleston-Front-End-May2015/members{/member}",
+  "public_members_url": "https://api.github.com/orgs/TIY-Charleston-Front-End-May2015/public_members{/member}",
+  "avatar_url": "https://avatars.githubusercontent.com/u/12236563?v=3",
+  "description": null
+  }
 ]
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   var demoNameAndDate = _.map(myDemos, function(el) {
     return {
        name: el.name,
-       date: el.updated_at
+       date: el.updated_at,
+       language: el.language
      }
   });
 
@@ -11,6 +12,22 @@ $(document).ready(function() {
 
   _.each(demoNameAndDate, function(el){
     $('.repos').append(demos(el));
+  });
+
+
+
+<!--separation//////////////////////////////////-->
+
+  var organ = _.map(myOrgs, function(el) {
+    return {
+      avatar: el.avatar_url
+    }
+  });
+
+  var myOrganizations = _.template($('#myOrganizations').html());
+
+  _.each(organ, function(el){
+    $('.organizations').append(myOrganizations(el));
   });
 
 });
